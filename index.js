19644499413b62ba475977f4fcc87702e5ccec07
Paiiss <pais.2k20@gmail.com>
2022-12-22 02:17:02 +0700

    Update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { Client } = require('discord.js-selfbot-v13');
 const config = require('./config.json')
 
 // Config
-const TOKEN = JSON.parse(process.env.TOKEN)
+const TOKEN = JSON.parse(process.env.TOKEN) || process.env.TOKEN
 const reportChannelId = config.reportChannelId || config.channelId
 const authorId = config.id, owoId = config.owoId
 
@@ -12,15 +12,16 @@ const random = (number = 10) => {
     return Math.floor(Math.random() * number)
 }
 
+// Can run many bots depending on device capacity
 for (const token of TOKEN) {
     let statusBot = true
-    let channelId = token.split('#')[1] || config.channelId // (Default channel for send owo)
+    let channelId = token.split('xxxxx')[1] || config.channelId // (Default channel for send owo)
 
     // sendOwo function along with random extra time so it is not detected (trial)
     const sendOwo = (text, time = 15000) => {
         setTimeout(async () => {
             if (!statusBot) return sendOwo(text, time)
-            console.log(`Send ${text}`)
+            console.log(`(${client.user.username}) send ${text}`)
             let channel = client.channels.cache.get(channelId);
             await channel.send(text)
             sendOwo(text, time)
@@ -31,8 +32,15 @@ for (const token of TOKEN) {
         checkUpdate: false,
     })
 
+    // To log in first, is there a simpler way?
+    async function login() {
+        // await client.login(token.split('xxxxx')[0]);
+    }
+    login()
+
     client.on('ready', async () => {
         console.log(`(${client.user.tag}) ready for auto farming`);
+        client.user.setActivity({ name: "auto farming owo", type: 'PLAYING' })
     })
 
     client.on('messageCreate', async (msg) => {
@@ -42,25 +50,31 @@ for (const token of TOKEN) {
                     statusBot = false
                     let getChannel = await client.channels.cache.get(reportChannelId)
                     let attachments = msg.attachments.map(e => e)
-                    if (attachments.length == 0) return getChannel.send(`**[MSG]** ${msg.content}\n ${msg.guild ? `**[Server]** : ${msg.guild.name} (${msg.channel.name})` : ''}`)
-                    return getChannel.send(`**[OwO]** ${msg.content}\nServer : [ ${msg.guild ? `**[Server]** : ${msg.guild.name} (${msg.channel.name})` : ''} ]\n**Link** : ${_dlink[0].url || 'Null'}\n<@${authorId}>`)
+                    if (attachments.length == 0) return getChannel.send(`**[MSG]** ${msg.content}\n ${msg.guild ? `**[Server]** : ${msg.guild.name} (${msg.channel.name})\n<@${authorId}>` : ''}`)
+                    return getChannel.send(`**[OwO]** ${msg.content}\nServer : [ ${msg.guild ? `**[Server]** : ${msg.guild.name} (${msg.channel.name})` : ''} ]\n**Link** : ${attachments[0].url || 'Null'}\n<@${authorId}>`)
                 }
             }
         }
 
-        if (msg.content.match(/👍|verified|Thank/g) && msg.author.id == `${owoId}` && msg.channel.type == 'dm') {
+        if (msg.content.match(/👍|verified|Thank you! :3/g) && msg.author.id == owoId && msg.channel.type == 'DM') {
             statusBot = true
             console.log(`(${client.user.tag}) Continue bots`)
+            return
         }
 
         if (msg.content.match(/!say/g) && msg.author == config.id) {
-            msg.channel.send(msg.content.replace('!say ', ''))
+            return msg.channel.send(msg.content.replace('!say ', ''))
         }
 
         // Message from Owo
-        if (msg.author.id == `${owoId}` && msg.channel.type == 'dm') {
-            let getId = await client.users.cache.get(config.authorId)
-            getId.send(`**[OWO SAY]** ${msg.content}`)
+        if (msg.author.id == owoId && msg.channel.type == 'DM') {
+            let getId = client.users.cache.get(config.authorId)
+            return getId.send(`**[OWO SAY]** ${msg.content}`)
+        }
+
+        if (msg.content.match(new RegExp(`!${client.user.username}`))) {
+            let getId = client.users.cache.get(owoId)
+            getId.send(msg.content.replace(`!${client.user.username} `, '')).catch((e) => console.log('Unable to send chat to guys'))
         }
     })
 
@@ -71,13 +85,11 @@ for (const token of TOKEN) {
     sendOwo('owob')
 
     // Owo coin flip
-    sendOwo(`owocf 10`, 20000)
+    sendOwo(`owocf 5`, 300000)
 
     // Owo pray
     sendOwo('owopray', 300000)
 
     // Owo cash
     sendOwo('owocash', 60000)
-
-    client.login(token.split('#')[0]);
 }
